5f07d4914e5dab83b3cef209631eb5016a8ea241
Roman <roman.kocenko.2004@mai.ru>
2022-10-19 01:54:23 +0300

    Create a few push to all users

--- app/controllers/controller.go
+++ app/controllers/controller.go
@@ -13,10 +13,15 @@ import (
 )
 
 type Controller struct {
+	Pushes    []string
+	FromEmail string
 }
 
 func New() *Controller {
-	return &Controller{}
+	return &Controller{
+		Pushes:    []string{"Hello, you won a bicycle!", "Whats'up, we can help you in education!"},
+		FromEmail: "roman.kocenko@mail.ru",
+	}
 }
 
 func (ctr *Controller) StartSending(cache *mailCache.Cache) {
@@ -31,40 +36,45 @@ func (ctr *Controller) StartSending(cache *mailCache.Cache) {
 		return
 	}
 
-	for _, email := range emails {
-		newMail := models.Mail{
-			From:    "roman.kocenko@mail.ru",
-			To:      email.Address,
-			Message: "Push advertissment",
-			IsRead:  false,
-		}
+	for _, push := range ctr.Pushes {
+		for _, email := range emails {
+			newMail := models.Mail{
+				From:    ctr.FromEmail,
+				To:      email.Address,
+				Message: push,
+				IsRead:  false,
+			}
 
-		buf := &bytes.Buffer{}
+			buf := &bytes.Buffer{}
 
-		err = tpl.Execute(buf, newMail)
-		if err != nil {
-			log.Println(err)
-			return
-		}
+			err = tpl.Execute(buf, newMail)
+			if err != nil {
+				log.Println(err)
+				return
+			}
 
-		bodyReader := bytes.NewReader(buf.Bytes())
-
-		req, err := http.NewRequest(http.MethodPost, "http://localhost:8081/mail/"+email.Address, bodyReader)
-		if err != nil {
-			fmt.Printf("client: could not create request: %s\n", err)
-			os.Exit(1)
-		}
-		req.Header.Set("Content-Type", "application/json")
-
-		client := http.Client{
-			Timeout: 30 * time.Second,
-		}
-
-		_, err = client.Do(req)
-		if err != nil {
-			fmt.Printf("client: error making http request: %s\n", err)
-			os.Exit(1)
+			PostRequest(buf, email.Address)
 		}
 	}
+}
 
+func PostRequest(buf *bytes.Buffer, emailAddress string) {
+	bodyReader := bytes.NewReader(buf.Bytes())
+
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:8081/mail/"+emailAddress, bodyReader)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+		os.Exit(1)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	_, err = client.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		os.Exit(1)
+	}
 }
--- client-mail/mailRouters/router.go
+++ client-mail/mailRouters/router.go
@@ -3,7 +3,6 @@ package mailRouters
 import (
 	"github.com/gorilla/mux"
 	"github.com/sQUARys/TestTaskMailGaner/client-mail/mailControllers"
-	"log"
 )
 
 type Router struct {
@@ -20,11 +19,9 @@ func New(controller *mailControllers.MailController) *Router {
 }
 
 func (r *Router) SetRoutes() {
-	log.Println("Mail routes start listening.")
 	r.Router.HandleFunc("/mail/{email}", r.Controller.MailHandler).Methods("Post")
+
 	r.Router.HandleFunc("/mail/sending-emails", r.Controller.GetMails).Methods("Get")
 	r.Router.HandleFunc("/mail/got-emails/{email}", r.Controller.GetMailsByEmail).Methods("Get")
 	r.Router.HandleFunc("/mail/message/{message-id:[0-9]+}", r.Controller.GetMailById).Methods("Get")
 }
-
-//:`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`
